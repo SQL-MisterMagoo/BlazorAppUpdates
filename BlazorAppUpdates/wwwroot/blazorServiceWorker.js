1938194a9dce62849b9fc516404731000b50372e
Mister Magoo <mister.magoo+githubsql@gmail.com>
2019-01-25 01:03:09 +0000

    Offline first complete

--- BlazorAppUpdates/wwwroot/blazorServiceWorker.js
+++ BlazorAppUpdates/wwwroot/blazorServiceWorker.js
@@ -1,11 +1,38 @@
 ﻿const filesToCache = [
-    '/_framework/_bin/BlazorAppUpdates.dll'
+    '/_framework/_bin/BlazorAppUpdates.dll',
+    '/_framework/_bin/Microsoft.AspNetCore.Blazor.Browser.dll',
+    '/_framework/_bin/Microsoft.AspNetCore.Blazor.dll',
+    '/_framework/_bin/Microsoft.AspNetCore.Blazor.TagHelperWorkaround.dll',
+    '/_framework/_bin/Microsoft.Extensions.DependencyInjection.Abstractions.dll',
+    '/_framework/_bin/Microsoft.Extensions.DependencyInjection.dll',
+    '/_framework/_bin/Microsoft.JSInterop.dll',
+    '/_framework/_bin/Mono.WebAssembly.Interop.dll',
+    '/_framework/_bin/mscorlib.dll',
+    '/_framework/_bin/System.Core.dll',
+    '/_framework/_bin/System.dll',
+    '/_framework/_bin/System.Net.Http.dll',
+    '/_framework/_bin/BlazorAppUpdates.pdb',
+    '/_framework/_bin/BlazorAppUpdates.dll',
+    '/_framework/wasm/mono.js',
+    '/_framework/wasm/mono.wasm',
+    '/_framework/blazor.boot.json',
+    '/_framework/blazor.webassembly.js',
+    '/css/bootstrap/bootstrap.min.css',
+    '/css/open-iconic/font/css/open-iconic-bootstrap.min.css',
+    '/css/site.css',
+    '/favicon.ico',
+    '/index.html',
+    '/blazorCache.js',
+    '/blazorSWRegister.js'
 ];
 
-const staticCacheName = 'blazor-cache-v1';
+const staticCacheName = 'blazor-cache-v2';
+const expectedCaches = [
+    staticCacheName
+];
 
 self.addEventListener('install', event => {
-    console.log('Attempting to install service worker and cache static assets');
+    self.skipWaiting();
     event.waitUntil(
         caches.open(staticCacheName)
             .then(cache => {
@@ -14,8 +41,27 @@ self.addEventListener('install', event => {
     );
 });
 
+// remove caches that aren't in expectedCaches
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(keys => Promise.all(
+            keys.map(key => {
+                if (!expectedCaches.includes(key)) return caches.delete(key);
+            })
+        ))
+    );
+});
+
 self.addEventListener('fetch', event => {
     console.log('Fetch event for ', event.request.url);
+    var requestUrl = new URL(event.request.url);
+
+    if (requestUrl.origin === location.origin) {
+        if (requestUrl.pathname === '/') {
+            event.respondWith(caches.match('/index.html'));
+            return;
+        }
+    }
     event.respondWith(
         caches.match(event.request)
             .then(response => {
@@ -29,16 +75,14 @@ self.addEventListener('fetch', event => {
                     .then(response => {
                         // TODO 5 - Respond with custom 404 page
 
-                        // Remove the "IF" to cache everything!
-                        const pathname = new URL(event.request.url).pathname;
-                        if (filesToCache.includes(pathname)) {
+                        if (response.ok) {
+                            const pathname = requestUrl.pathname;
                             console.log("CACHE: Adding " + pathname);
                             return caches.open(staticCacheName).then(cache => {
                                 cache.put(event.request.url, response.clone());
                                 return response;
                             });
                         }
-                        return response;
                     });
             }).catch(error => {
 
